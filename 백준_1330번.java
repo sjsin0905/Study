fdb3c34e1d5a6e75d28bd8745b959de3aac4d3f2
조용훈 <sjsin0905@naver.com>
2023-04-29 13:27:54 +0900

    Create 백준_1330번.java

---
+++ 백준_1330번.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class 백준_1330번 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        // .next() -> 공백 제거
+        // /nextLine() -> 공백 유지
+        String splitArr[] = num.split(" ");
+        int n1, n2;
+        n1 = Integer.parseInt(splitArr[0]);
+        n2 = Integer.parseInt(splitArr[1]);
+        if (n1 == n2) {
+            System.out.println("==");
+        }
+        else if (n1 > n2) {
+            System.out.println(">");
+        }
+        else {
+            System.out.println("<");
+        }
+
+    }
+}
